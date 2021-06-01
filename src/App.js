ad69b691f44e982cf540cfc9e31e98511707f76b
FadelaTaam <taam-fadela@live.com>
2021-06-01 19:40:44 +0200

    consigne 5

--- src/App.js
+++ src/App.js
@@ -3,28 +3,53 @@ import './App.css';
 import Box from "./components/Box.jsx";
 import "./styles/global.css";
 
-  const tempMin= -20
-  const tempMax=40
-  const heartMin= 80
-  const heartMax= 180
-  const stepsMin=0
-  const stepsMax= 50000
+const tempMin = -20
+const tempMax = 40
+const heartMin = 80
+const heartMax = 180
+const stepsMin = 0
+const stepsMax = 50000
 
- export default class App extends React.Component {
-  
+export default class App extends React.Component {
+
   render() {
     return (
       <div class="container-fluid">
-      
+
         <div class="row">
-          <Box/>
+          <Box
+            //boîte Water//
+            const icon="local_drink"
+            const color="#3A85FF"
+            const value={1.5}
+            const unit="L" />
+          <Box
+            //boîte Steps// 
+            const icon="directions_walk"
+            color="black"
+            value={3000}
+            unit="steps" />
+          <Box
+            //boîte Heart//
+            icon="favorite"
+            color="red"
+            value={120}
+            unit="bpm" />
+          <Box
+            //boîte Temperature//
+            icon="wb_sunny"
+            color="yellow"
+            value={-10}
+            unit="°C" />
+
+
           <p> Heart :{heartMin}</p>
-          <p> Temperature :{tempMin}</p> 
+          <p> Temperature :{tempMin}</p>
           <p> Steps :{stepsMin}</p>
-          
+
         </div>
-        
+
       </div>
     );
   }
-} 
+}
