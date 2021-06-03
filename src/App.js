72acdd46498a19a3c992f87d9e2319ea588bef3d
FadelaTaam <taam-fadela@live.com>
2021-06-03 09:41:45 +0200

    consigne 8

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
+import './styles/global.css';
 import './App.css';
-import Box from "./components/Box.jsx";
-import "./styles/global.css";
+import Box from './components/Box.jsx';
 
 const tempMin = -20
 const tempMax = 40
@@ -10,49 +10,72 @@ const heartMax = 180
 const stepsMin = 0
 const stepsMax = 50000
 
-export default class App extends React.Component {
-  constructor () {
+class App extends React.Component {
+  constructor() {
     super();
     this.state = {
-      water : 0,
-      heart : 120,
-      temperature : -10,
-      steps  : 3000
+      water:1.5, 
+      heart: 120, 
+      temperature: -10, 
+      steps:3000
     };
   }
+  onTempChange = (e) => {
+    this.setState({ temperature: e.target.value });
+  }; 
+  onHeartChange = (e) => {
+  this.setState({ heart: e.target.value });
+};
+  onStepsChange = (e) => {
+    this.setState({ steps: e.target.value });
+  };
+  
 
   render() {
     return (
-      <div class="container-fluid">
-
-        <div class="row">
+      <div className="container-fluid">
+        <div className="row">
+          <Box icon="local_drink" 
+          color="#3A85FF" 
+          value={1.5} 
+          unit="L" 
+          />
           <Box
-            //boîte Water//
-            const icon="local_drink"
-            const color= "#3A85FF"
-            const value={1.5}
-            const unit="L" />
-          <Box
-            //boîte Steps// 
-            const icon="directions_walk"
+            icon="directions_walk"
             color="black"
-            value={3000}
-            unit="steps" />
-          <Box
-            //boîte Heart//
-            const icon="favorite"
-            const color="red"
-            value={120}
-            unit="bpm" />
-          <Box
-            //boîte Temperature//
-            icon="wb_sunny"
-            color="yellow"
-            value={-10}
-            unit="°C" />
+            value={this.state.steps}
+            unit="steps"
+            min={stepsMin}
+            max={stepsMax}
+            onChange={this.onStepsChange}
+          />
+          <Box icon="favorite" 
+          color="red" 
+          value={this.state.heart} 
+          unit="bpm"
+          min={heartMin}
+          max={heartMax}
+          onChange={this.onHeartChange}
+          />
+          <Box icon="wb_sunny" 
+          color="yellow" 
+          value={this.state.temperature} 
+          unit="°C" 
+          min={tempMin}
+          max={tempMax}
+          onChange={this.onTempChange}
+          />
         </div>
-
       </div>
     );
   }
 }
+export default App;
+
+/*
+Quelque soit les éléments externes, vous devez boire au minimum 1,5 litre d’eau par jour
+Si la température est supérieure à 20 degrés Celsius, chaque degré au dessus aura un facteur de 0,02 litres en plus
+Si les battements de coeurs sont au dessus de 120, chaque battement au dessus aura un facteur de 0,0008 litres en plus
+Si le nombre de pas est supérieur à 10 000, chaque pas au dessus aura un facteur de 0,00002 litres en plus
+*/
+ 
--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -5,16 +5,17 @@ class Box extends React.Component {
     render() {
         let slider
         if (this.props.icon !== "local_drink") {
-            slider = <input type="range" id="volume" name="jauge" min="0" max="11"></input>
+            slider = <input type="range" min={this.props.min} max={this.props.max} onChange={this.props.onChange} value={this.props.value}></input>
         } else { slider = null }
         return (
             <div className="box col-sm-3 col-6">
                 <span className="material-icons"
-                style={{ fontSize: 100, color: this.props.color }}>
+                    style={{ fontSize: 100, color: this.props.color }}>
+
                     {this.props.icon}</span>
 
                 <p>{this.props.value} {this.props.unit}</p>
-                
+
                 <div>
                     {slider}
                 </div>
